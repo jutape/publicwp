ab56cd0c65e81aebe00c83014cca1aa93dc32683
jutape <joaojoao.jopdsr@gmail.com>
2019-10-06 15:50:07 -0300

    adicionando nome da pessoa que digitou

--- public/javascripts/socket.js
+++ public/javascripts/socket.js
@@ -15,7 +15,7 @@ $(() => {
     let newUser = $("<div>");
     newUser.addClass("info");
     newUser.text(`${people} se conectou`);
-    addToStatus(users)
+    addToStatus(users);
     $(".conversation-container").append(newUser);
   });
 
@@ -23,25 +23,29 @@ $(() => {
     let deleteUser = $("<div>");
     deleteUser.addClass("info");
     deleteUser.text(`${people} se desconectou`);
-    addToStatus(users)
+    addToStatus(users);
     $(".conversation-container").append(deleteUser);
   });
 
   socket.on("chat message", (msg, pessoa) => {
     let message = $("<div>");
     message.addClass("message");
-    if(pessoa === people){
-      message.addClass("sent");
-    }else {
-      message.addClass("received");
-    }
     message.text(msg);
+    if (pessoa === people) {
+      message.addClass("sent");
+    } else {
+      message.addClass("received");
+      let contato = $("<p>");
+      contato.addClass("contact");
+      contato.text(people);
+      message.prepend(contato);
+    }
     $(".conversation-container").append(message);
   });
 
-  const addToStatus = (users) => {
-    if(Array.isArray(users)){
-      $('.status').text(users.join(', '));
+  const addToStatus = users => {
+    if (Array.isArray(users)) {
+      $(".status").text(users.join(", "));
     }
   };
 });
